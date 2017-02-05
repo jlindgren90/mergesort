fbbfab45b77bcc29d4c837175d3516f8c2c1d920
John Lindgren <john.lindgren@aol.com>
2017-02-05 07:06:40 -0500

    Factor out do_merge.

--- mergesort.c
+++ mergesort.c
@@ -4,6 +4,34 @@
 static void * buf = NULL;
 static int buf_size = 0;
 
+void do_merge (void * items, int half_size, int full_size,
+               int size, GCompareDataFunc compare, void * data)
+{
+    if (buf_size < half_size)
+    {
+        buf = g_realloc (buf, half_size);
+        buf_size = half_size;
+    }
+
+    memcpy (buf, items, half_size);
+
+    const void * a = buf;
+    const void * b = items + half_size;
+    const void * a_end = buf + half_size;
+    const void * b_end = items + full_size;
+
+    for (void * dest = items; dest < b_end; dest += size)
+    {
+        if (b >= b_end || (a < a_end && compare (a, b, data) < 1)) {
+            memcpy (dest, a, size);
+            a += size;
+        } else {
+            memcpy (dest, b, size);
+            b += size;
+        }
+    }
+}
+
 void mergesort (void * items, int n_items, int size,
                 GCompareDataFunc compare, void * data)
 {
@@ -11,34 +39,10 @@ void mergesort (void * items, int n_items, int size,
         return;
 
     int half = n_items / 2;
-    int half_size = half * size;
-    int full_size = n_items * size;
-
-    if (buf_size < half_size)
-    {
-        buf = g_realloc (buf, half_size);
-        buf_size = half_size;
-    }
 
     mergesort (items, half, size, compare, data);
-    mergesort (items + half_size, n_items - half, size, compare, data);
+    mergesort (items + half * size, n_items - half, size, compare, data);
 
-    memcpy (buf, items, half_size);
-
-    const void * a = buf;
-    const void * b = items + half_size;
-
-    void * end = items + full_size;
-    void * buf_end = buf + half_size;
-
-    for (void * c = items; c < end; c += size)
-    {
-        if (b >= end || (a < buf_end && compare (a, b, data) < 1)) {
-            memcpy (c, a, size);
-            a += size;
-        } else {
-            memcpy (c, b, size);
-            b += size;
-        }
-    }
+    do_merge (items, half * size, n_items * size,
+              size, compare, data);
 }
