77972bc603e367c21387e7ae8e2385c4661d6a5c
John Lindgren <john@jlindgren.net>
2019-02-21 00:42:16 -0500

    Eliminate the MergeSort class.

--- cpp/mergesort.h
+++ cpp/mergesort.h
@@ -35,14 +35,13 @@
  */
 
 template<typename Iter, typename Less>
-class MergeSort
+void mergesort (Iter start, Iter end, Less less)
 {
-private:
     typedef typename std::iterator_traits<Iter>::value_type Value;
 
     /* One step of an insertion sort: rotates the head item into place
      * within the sorted sub-list [head + 1, tail) */
-    static void rotate_head (Iter head, Iter tail, Less less)
+    auto rotate_head = [less] (Iter head, Iter tail)
     {
         /* Find the proper location for the head item.  Skip *(head+1)
          * since we already know it is less than *(head). */
@@ -54,10 +53,10 @@ private:
         Value tmp = std::move (* head);
         std::move (head + 1, dest, head);
         * (dest - 1) = std::move (tmp);
-    }
+    };
 
     /* Merges the two sorted sub-lists [head, mid) and [mid, tail) */
-    static void do_merge (Iter head, Iter mid, Iter tail, Less less, std::vector<Value> & buf)
+    auto do_merge = [less] (Iter head, Iter mid, Iter tail, std::vector<Value> & buf)
     {
         /* Copy list "a" to temporary storage.  Move items directly onto
          * the existing vector if it's big enough.  Otherwise, create a
@@ -92,131 +91,122 @@ private:
 
         /* copy remainder of list "a" */
         std::move (a, a_end, dest);
-    }
+    };
 
-public:
-    /* Top-level merge sort algorithm */
-    static void sort (Iter start, Iter end, Less less)
+    /* A list with 0 or 1 element is sorted by definition. */
+    if (end - start < 2)
+        return;
+
+    std::vector<Value> buf;
+
+    /* The algorithm runs right-to-left (so that insertions are left-to-right). */
+    Iter head = end;
+
+    /* Markers recording the divisions between sorted sub-lists or "runs".
+     * Each run is at least 2x the length of its left-hand neighbor, so in
+     * theory a list of 2^64 - 1 elements will have no more than 64 runs. */
+    Iter div[64];
+    int n_div = 0;
+
+    do
     {
-        /* A list with 0 or 1 element is sorted by definition. */
-        if (end - start < 2)
-            return;
+        Iter mid = head;
+        head --;
 
-        std::vector<Value> buf;
-
-        /* The algorithm runs right-to-left (so that insertions are left-to-right). */
-        Iter head = end;
-
-        /* Markers recording the divisions between sorted sub-lists or "runs".
-         * Each run is at least 2x the length of its left-hand neighbor, so in
-         * theory a list of 2^64 - 1 elements will have no more than 64 runs. */
-        Iter div[64];
-        int n_div = 0;
-
-        do
+        /* Scan right-to-left to find a run of increasing values.
+         * If necessary, use insertion sort to create a run at 4 values long.
+         * At this scale, insertion sort is faster due to lower overhead. */
+        while (head > start)
         {
-            Iter mid = head;
-            head --;
-
-            /* Scan right-to-left to find a run of increasing values.
-             * If necessary, use insertion sort to create a run at 4 values long.
-             * At this scale, insertion sort is faster due to lower overhead. */
-            while (head > start)
+            if (less (* head, * (head - 1)))
             {
-                if (less (* head, * (head - 1)))
-                {
-                    if (mid - head < 4)
-                        rotate_head (head - 1, mid, less);
-                    else
-                        break;
-                }
-
-                head --;
+                if (mid - head < 4)
+                    rotate_head (head - 1, mid);
+                else
+                    break;
             }
 
-            /* Merge/collapse sub-lists left-to-right to maintain the invariant. */
-            while (n_div >= 1)
+            head --;
+        }
+
+        /* Merge/collapse sub-lists left-to-right to maintain the invariant. */
+        while (n_div >= 1)
+        {
+            Iter tail = div[n_div - 1];
+
+            while (n_div >= 2)
             {
-                Iter tail = div[n_div - 1];
-
-                while (n_div >= 2)
-                {
-                    Iter tail2 = div[n_div - 2];
-
-                    /*
-                     * Check for the occasional case where the new sub-list is
-                     * longer than both the two previous.  In this case, a "3-way"
-                     * merge is performed as follows:
-                     *
-                     *   |---------- #6 ----------|- #5 -|---- #4 ----| ...
-                     *
-                     * First, the two previous sub-lists (#5 and #4) are merged.
-                     * (This is more balanced and therefore more efficient than
-                     * merging the long #6 with the short #5.)
-                     *
-                     *   |---------- #5 ----------|-------- #4 -------| ...
-                     *
-                     * The invariant guarantees that the newly merged sub-list (#4)
-                     * will be shorter than its right-hand neighbor (#3).
-                     *
-                     * At this point we loop, and one of two things can happen:
-                     *
-                     *  1) If sub-list #5 is no longer than #3, we drop out of the
-                     *     loop.  #5 is still longer than half of #4, so a 2-way
-                     *     merge will be required to restore the invariant.
-                     *
-                     *  2) If #5 is longer than even #3 (rare), we perform another
-                     *     3-way merge, starting with #4 and #3.  The same result
-                     *     holds true: the newly merged #3 will again be shorter
-                     *     than its right-hand neighbour (#2).  In this fashion the
-                     *     process can be continued down the line with no more than
-                     *     two sub-lists violating the invariant at any given time.
-                     *     Eventually no more 3-way merges can be performed, and the
-                     *     invariant is restored by a final 2-way merge.
-                     */
-
-                    if ((mid - head) <= (tail2 - tail))
-                        break;
-
-                    do_merge (mid, tail, tail2, less, buf);
-
-                    tail = tail2;
-                    n_div --;
-                }
+                Iter tail2 = div[n_div - 2];
 
                 /*
-                 * Otherwise, check whether the new sub-list is longer than half its
-                 * right-hand neighbour.  If so, merge the two sub-lists.  The
-                 * merged sub-list may in turn be longer than its own right-hand
-                 * neighbor, and if so the entire process is repeated.
+                 * Check for the occasional case where the new sub-list is
+                 * longer than both the two previous.  In this case, a "3-way"
+                 * merge is performed as follows:
                  *
-                 * Once the "head" pointer reaches the beginning of the original
-                 * list, we simply keep merging until only one sub-list remains.
+                 *   |---------- #6 ----------|- #5 -|---- #4 ----| ...
+                 *
+                 * First, the two previous sub-lists (#5 and #4) are merged.
+                 * (This is more balanced and therefore more efficient than
+                 * merging the long #6 with the short #5.)
+                 *
+                 *   |---------- #5 ----------|-------- #4 -------| ...
+                 *
+                 * The invariant guarantees that the newly merged sub-list (#4)
+                 * will be shorter than its right-hand neighbor (#3).
+                 *
+                 * At this point we loop, and one of two things can happen:
+                 *
+                 *  1) If sub-list #5 is no longer than #3, we drop out of the
+                 *     loop.  #5 is still longer than half of #4, so a 2-way
+                 *     merge will be required to restore the invariant.
+                 *
+                 *  2) If #5 is longer than even #3 (rare), we perform another
+                 *     3-way merge, starting with #4 and #3.  The same result
+                 *     holds true: the newly merged #3 will again be shorter
+                 *     than its right-hand neighbour (#2).  In this fashion the
+                 *     process can be continued down the line with no more than
+                 *     two sub-lists violating the invariant at any given time.
+                 *     Eventually no more 3-way merges can be performed, and the
+                 *     invariant is restored by a final 2-way merge.
                  */
 
-                if (head > start && (mid - head) <= (tail - mid) / 2)
+                if ((mid - head) <= (tail2 - tail))
                     break;
 
-                do_merge (head, mid, tail, less, buf);
+                do_merge (mid, tail, tail2, buf);
 
-                mid = tail;
+                tail = tail2;
                 n_div --;
             }
 
-            /* push the new sub-list onto the stack */
-            div[n_div] = mid;
-            n_div ++;
-        }
-        while (head > start);
-    }
-};
+            /*
+             * Otherwise, check whether the new sub-list is longer than half its
+             * right-hand neighbour.  If so, merge the two sub-lists.  The
+             * merged sub-list may in turn be longer than its own right-hand
+             * neighbor, and if so the entire process is repeated.
+             *
+             * Once the "head" pointer reaches the beginning of the original
+             * list, we simply keep merging until only one sub-list remains.
+             */
 
-template<typename Iter, typename Less>
-void mergesort (Iter start, Iter end, Less less)
-    { MergeSort<Iter, Less>::sort (start, end, less); }
+            if (head > start && (mid - head) <= (tail - mid) / 2)
+                break;
+
+            do_merge (head, mid, tail, buf);
+
+            mid = tail;
+            n_div --;
+        }
+
+        /* push the new sub-list onto the stack */
+        div[n_div] = mid;
+        n_div ++;
+    }
+    while (head > start);
+}
 
 template<typename Iter>
-void mergesort (Iter const start, Iter const end)
+void mergesort (Iter start, Iter end)
 {
     typedef typename std::iterator_traits<Iter>::value_type Value;
     mergesort (start, end, std::less<Value> ());
